2b32b14f6cfd87155a2b9acce21a3bd59676dd11
Renars <you@example.com>
2025-12-18 09:14:33 +0200

    3.uzdevums

--- about.php
+++ about.php
@@ -1,1 +1,24 @@
-<!-- your code goes here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>About</h1>";
+
+        $indexedArray = [
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto.",
+        ];
+
+        foreach($indexedArray as $element)
+        {
+            echo "<p>" . $element . "</p>";
+        }
+    ?>
+</body>
+</html>
